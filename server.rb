034301d18b062f0490d762dd022f0c476a8774f5
Rachael Serur <rachaelse@gmail.com>
2014-05-31 00:19:47 -0400

    Recipes and recipes/:id pages both have full functionality. Need to add style and basic navigation.

--- server.rb
+++ server.rb
@@ -1,3 +1,38 @@
 require 'sinatra'
 require 'pg'
 
+
+def get_recipes
+  connection = PG.connect(dbname: 'recipe_box')
+  results = connection.exec('SELECT * FROM recipes')
+  connection.close
+  results
+end
+
+def get_recipe_info(id)
+  connection = PG.connect(dbname: 'recipe_box')
+  sql = "SELECT * FROM recipes WHERE id = $1"
+  results = connection.exec(sql, [id])
+  connection.close
+  results
+end
+
+def get_ingredients(id)
+  connection = PG.connect(dbname: 'recipe_box')
+  sql = "SELECT * FROM ingredients WHERE recipe_id = $1"
+  results = connection.exec(sql, [id])
+  connection.close
+  results
+end
+
+get '/recipes' do
+  @recipes = get_recipes
+  erb :'recipes.html'
+end
+
+get '/recipes/:id' do
+  @recipe_id = params[:id]
+  @recipe_info = get_recipe_info(@recipe_id)
+  @ingredients = get_ingredients(@recipe_id)
+  erb :'recipe_info.html'
+end
